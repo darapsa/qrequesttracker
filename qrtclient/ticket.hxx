5b650c5f483ff98cea125606372ef433c53fc2a7
ꦌꦫꦶꦏ꧀ꦦꦿꦧꦮꦑꦩꦭ꧀ <erik@darapsa.co.id>
2022-02-02 10:20:52 +0800

    Reindent

--- qrtclient/ticket.hxx
+++ qrtclient/ticket.hxx
@@ -9,56 +9,51 @@ struct rtclient_search_ticket_list;
 
 namespace RTClient {
 
-	class Ticket
-	{
-		public:
-			Ticket(rtclient_ticket* ticket)
-				: m_id{ticket->id}
-				, m_subject{ticket->subject}
-			{}
-			unsigned int id() const { return m_id; }
-			QString const& subject() const { return m_subject; }
+class Ticket
+{
+	public:
+		Ticket(rtclient_ticket* ticket) :
+			m_id{ticket->id},
+			m_subject{ticket->subject}
+		{}
+		unsigned int id() const { return m_id; }
+		QString const& subject() const { return m_subject; }
 
-		private:
-			unsigned int m_id;
-			QString m_subject;
-	};
+	private:
+		unsigned int m_id;
+		QString m_subject;
+};
 
-	class TicketList : public QAbstractListModel
-	{
-		Q_OBJECT
-		Q_PROPERTY(int rowCount READ rowCount NOTIFY rowCountChanged)
+class TicketList : public QAbstractListModel
+{
+	Q_OBJECT
 
-		public:
-			enum TicketRoles {
-				IdRole = Qt::UserRole + 1,
-				SubjectRole
-			};
+	public:
+		enum TicketRoles {
+			IdRole = Qt::UserRole + 1,
+			SubjectRole
+		};
 
 			explicit TicketList(QObject* parent = nullptr)
 				: QAbstractListModel{parent} {}
 
-			int rowCount(QModelIndex const& parent
-					= QModelIndex()) const Q_DECL_OVERRIDE;
-			QVariant data(const QModelIndex& index
-					, int role = Qt::DisplayRole
-					) const Q_DECL_OVERRIDE;
+		int rowCount(QModelIndex const& parent
+				= QModelIndex()) const Q_DECL_OVERRIDE;
+		QVariant data(const QModelIndex& index,
+				int role = Qt::DisplayRole
+			     ) const Q_DECL_OVERRIDE;
 
-		public slots:
-			void update(rtclient_search_ticket_list* list);
-
-		protected:
-			QHash<int, QByteArray> roleNames() const Q_DECL_OVERRIDE;
+	protected:
+		QHash<int, QByteArray> roleNames() const Q_DECL_OVERRIDE;
 
 		signals:
 			void updated();
 			void rowCountChanged();
 
-		private:
-			QList<Ticket> tickets;
-			void addTicket(Ticket const& ticket);
-	};
+	private:
+		QList<Ticket> tickets;
+};
 
 }
 
-#endif // QRTCLIENT_TICKET_HXX
+#endif
